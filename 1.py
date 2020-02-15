edf3ee658b7d08fab4937d91362f15de579609ea
php frontant progrmare <40247827+Iliavas@users.noreply.github.com>
2020-02-15 10:34:15 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,8 @@
+num = int(input())
+
+l = list(map(lambda x: ' '.join(x), input()))
+s_l = []
+for i in l:
+    if i != ' ': s_l.append(i)
+l = list(map(lambda x: int(x), s_l))
+print(sum(l) // len(l))
