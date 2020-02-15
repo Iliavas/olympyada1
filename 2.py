cbbc9298e19d4b13b2eac611d0c08ccb85269bad
php frontant progrmare <40247827+Iliavas@users.noreply.github.com>
2020-02-15 11:45:55 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,3 +1,10 @@
+'''def num
+
+for i in range(200, 301):
+  if sum(list(map(lambda x: if x % 2 == 0: x else '', str(i))))
+
+  '''
+
 position, _  = input().split()
 
 dec = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
@@ -6,21 +13,21 @@ int_pos = dec.index(position[0]) + 1
 integer_pos = int(position[1])
 def predict_ch():
     res_list = []
-    if int_pos - 2 >= 0 and integer_pos - 1 >= 0:
+    if int_pos - 2 >= 0 and integer_pos - 1 > 0:
         res_list.append(dec[int_pos - 3] + str(integer_pos - 1))
-    if int_pos - 1 >= 0 and integer_pos - 2 >= 0:
+    if int_pos - 1 >= 0 and integer_pos - 2 > 0:
         res_list.append(dec[int_pos - 2] + str(integer_pos - 2))
-    if int_pos + 1 <= 8 and integer_pos - 2 >= 0:
+    if int_pos + 1 <= 8 and integer_pos - 2 > 0:
         res_list.append(dec[int_pos] + str(integer_pos - 2))
-    if int_pos + 2 <= 8 and integer_pos - 1 >= 0:
+    if int_pos + 2 <= 8 and integer_pos - 1 > 0:
         res_list.append(dec[int_pos + 2] + str(integer_pos - 1))
     if int_pos + 1 <= 8 and integer_pos + 2 <= 8:
         res_list.append(dec[int_pos + 1] + str(integer_pos + 2))
     if int_pos + 2 <= 8 and integer_pos + 1 <= 8:
         res_list.append(dec[int_pos + 2] + str(integer_pos + 1))
-    if int_pos - 1 >= 0 and integer_pos + 2 <= 8:
+    if int_pos - 1 > 0 and integer_pos + 2 <= 8:
         res_list.append(dec[int_pos - 2] + str(integer_pos + 2))
-    if int_pos - 2 >= 0 and integer_pos + 1 <= 8:
+    if int_pos - 2 > 0 and integer_pos + 1 <= 8:
         res_list.append(dec[int_pos - 3] + str(integer_pos + 1))
   
     return res_list
