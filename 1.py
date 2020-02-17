e9132155245170a01cdebb23c45fb360594d4694
Ilia vasilenko <40247827+Iliavas@users.noreply.github.com>
2020-02-17 17:03:33 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,8 @@
-num = int(input())
+'''num = int(input())
 
 l = list(map(lambda x: ' '.join(x), input()))
-s_l = []
+#s_l = []
 for i in l:
-    if i != ' ': s_l.append(i)
+    #if i != ' ': s_l.append(i)
 l = list(map(lambda x: int(x), s_l))
 print(sum(l) // len(l))
