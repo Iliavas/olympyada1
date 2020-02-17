080aa83d5ff0737c4d052464af6af7b3eda52952
Ilia vasilenko <40247827+Iliavas@users.noreply.github.com>
2020-02-17 17:05:05 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -5,12 +5,12 @@ for i in range(a):
     r = input()
     
     if 'drove into place' in r:
-        b[r.split()[0]] = r.split()[-1]
-    elif 'drove out' in r:
+        #b[r.split()[0]] = r.split()[-1]
+    elif 'drave out' in r:
         del b[r.split()[0]]
     else:
         r = r.split()
         if r[-1] in b:
-            print(b[r[-1]])
+            print(b[r[1]])
         else:
-            print('Not here')
+            print('Not hre ')
